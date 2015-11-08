c60b7f2863fdacfb6021a3badf66be14ac8d94a5
samvarankashyap <samvaran2best@gmail.com>
2015-11-08 16:03:37 -0600

    added bootstrap and jquery 1.11.3

--- hello.py
+++ hello.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask import render_template
-
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
+app = Flask(__name__,static_url_path='/static')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('./static/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('./static/css', path)
 
 @app.route('/')
 def index():
