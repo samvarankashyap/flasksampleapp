d9643a50f35180b45e234ace8fda5db94b888770
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2017-03-02 19:15:56 -0500

    Updated the newlines between functions

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import Flask, request, send_from_directory
+
 app = Flask(__name__,static_url_path='/static')
 
 @app.route('/hello/')
@@ -13,9 +14,11 @@ def hello(name=None):
 def send_js(path):
     return send_from_directory('./static/js', path)
 
+
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('./static/css', path)
+
 
 @app.route('/')
 def index():
@@ -27,18 +30,22 @@ def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
 
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
 
+
 @app.route('/about')
 def about():
     return 'The about page'
+
 
 if __name__ == '__main__':
     app.debug = True
